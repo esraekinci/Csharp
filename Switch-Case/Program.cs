398a3d90d00d88a960a46883a940a956c827d1a2
Esra Ekinci <esraekinci07@gmail.com>
2022-11-03 15:56:50 +0300

    Switch-Case Statement added
    Switch-Case usage.

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            // Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                break;
+                case 2:
+                    Console.WriteLine("February");
+                break;
+                case 3:
+                    Console.WriteLine("March");
+                break;
+                case 5:
+                    Console.WriteLine("May");
+                break;
+                case 4:
+                    Console.WriteLine("April");
+                break;                   
+                
+                default:
+                    Console.WriteLine("Unknown Data");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10: 
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+                
+            }
+        }
+    }
+}
