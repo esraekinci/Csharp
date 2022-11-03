9a61489af4c610bfcf3487725300e23af6105120
Esra Ekinci <esraekinci07@gmail.com>
2022-11-03 15:28:01 +0300

    Conditions added
    If, ElseIf condition samples are added.

---
+++ Conditions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int time = DateTime.Now.Hour;
+
+           if(time >= 6 && time <= 11)
+                Console.WriteLine("Good Morning!");
+
+            else if(time <= 18)
+                Console.WriteLine("Have a Nice day!");
+
+            else
+                Console.WriteLine("Good Night!");
+                
+
+            // Same thing but different way    
+            string result = time >= 6 && time <= 11 ? "Good Morning!" : time <= 18 ? "Have a Nice Day!" : "Good Night!";
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
