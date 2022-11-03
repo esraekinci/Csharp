a7a02e235cc7e4d68075a5b97edad0b83311e598
Esra Ekinci <esraekinci07@gmail.com>
2022-11-03 17:23:17 +0300

    While Loop and ForEach added
    While Loop and Foreach usage and samples

---
+++ Loops_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Loops_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While Loop
+            // 1'den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayarak console'a yazdır.
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            int counter=1;
+            int sum=0;
+            while (counter <= number)
+            {
+                sum += counter;
+                counter ++;
+            }
+            Console.WriteLine($"Summation: {sum}");
+            Console.WriteLine("Average:" + sum/number);
+
+            
+
+            //Foreach Sample
+            Console.WriteLine("---Foreach---");
+
+            string[] cars= {"BMW","Audi","Mercedes"};
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
